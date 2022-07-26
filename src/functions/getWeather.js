9c0767405e79a8d854faf687c6a473c7952406e9
Michal Leskiewicz <leskiewicz02@gmail.com>
2022-07-26 09:35:37 +0200

    http through cors-anywhere

--- src/functions/getWeather.js
+++ src/functions/getWeather.js
@@ -2,13 +2,13 @@ async function getWeather(city) {
   try {
     // fetch weather info from other website with the city inside input
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=ed7a5619e14a98cd13e091aef92cc5ae`,
+      `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=ed7a5619e14a98cd13e091aef92cc5ae`,
       { mode: 'cors' }
     );
     // check if server didn't throw an error
     if (!response.ok) {
       document.querySelector('#weatherText').innerText =
-        'Couldn\t search for the city.Ł';
+        'Couldn\t search for the city.';
       throw new Error('Bad response from server');
     }
     const data = await response.json();
